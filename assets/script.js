e499ba600d67dc262503ae5a4d12d8da7f590f10
Peter Bishop <peterbishop@Peters-MacBook-Air.local>
2021-05-04 16:20:49 -0600

    99% funtional version

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,11 @@
 var appID = "879fa5a3379aff15dec6456e147800b1";
 var idList = [];
+var fiveDayIcon = [];
+var fiveDayTemp = [];
+var fiveDayWind = [];
+var fiveDayHumidity = [];
+var buttonPressed = false;
+var historicalCity = "";
 
 var currentCity = $("#currentCity");
 var currentTemp = $("#currentTemp");
@@ -9,23 +15,56 @@ var currentUV = $("#currentUV");
 
 //add event listener for search button press
 
-var searchButton = $("#searchButton");
-searchButton.on("click", function (event) {
-  event.preventDefault();
-  search();
+// var searchButton = $("#searchButton");
+// searchButton.on("click", function () {
+//   search();
+// });
+
+var historyDiv = $("#wrapper");
+historyDiv.on("click", function (event) {
+  var target = event.target;
+  console.log(target);
+  if (target.nodeName == "BUTTON") {
+    buttonPressed = true;
+    console.log(event.target);
+    historicalCity = event.target.id;
+    console.log("Event target ID: " + historicalCity);
+    search();
+  } else if (target.id == "searchButton") {
+    search();
+  } else {
+    console.log("nothing");
+    console.log(buttonPressed);
+  }
 });
 
-var histButtonPressed = $(".histButton");
-histButtonPressed.on("click", function (event) {
-  event.preventDefault();
-  console.log("searched item pressed");
-  search();
-});
+// var histButtonPressed = $(".histButton");
+// histButtonPressed.on("click", function (event) {
+//   console.log("searched item pressed");
+//   search();
+// });
 
 //function to create buttons based on search
 
 function search() {
-  var searchInput = document.getElementById("searchInput").value;
+  var searchInput = "";
+  if (buttonPressed == true) {
+    console.log("button pressed true in search function");
+    searchInput = historicalCity;
+    idList.splice(0, idList.length);
+    fiveDayTemp.splice(0, fiveDayTemp.length);
+    fiveDayWind.splice(0, fiveDayWind.length);
+    fiveDayHumidity.splice(0, fiveDayHumidity.length);
+    fiveDayIcon.splice(0, fiveDayIcon.length);
+  } else {
+    searchInput = document.getElementById("searchInput").value;
+    idList.splice(0, idList.length);
+    fiveDayTemp.splice(0, fiveDayTemp.length);
+    fiveDayWind.splice(0, fiveDayWind.length);
+    fiveDayHumidity.splice(0, fiveDayHumidity.length);
+    fiveDayIcon.splice(0, fiveDayIcon.length);
+  }
+
   var searchHistory = document.getElementById("searchHistoryButtons");
   var button = document.createElement("button");
   var buttonName = document.createTextNode(searchInput);
@@ -33,17 +72,18 @@ function search() {
   if (searchInput == "") {
     alert("You must enter a valid city name!");
   } else {
-    console.log(buttonName);
-    button.className = "form-control histButton btn-secondary";
-    button.id = searchInput;
-    button.appendChild(buttonName);
-    searchHistory.appendChild(button);
-    idList.push(button.id);
-    console.log(idList);
-    localStorage.setItem("city", JSON.stringify(idList));
-    var name = JSON.parse(localStorage.getItem("city"));
-    console.log(name + " saved");
-
+    if (buttonPressed == false) {
+      console.log(buttonName);
+      button.className = "form-control histButton btn-secondary";
+      button.id = searchInput;
+      button.appendChild(buttonName);
+      searchHistory.appendChild(button);
+      idList.push(button.id);
+      console.log(idList);
+      localStorage.setItem("city", JSON.stringify(idList));
+      var name = JSON.parse(localStorage.getItem("city"));
+      console.log(name + " saved");
+    }
     var latValue;
     var lonValue;
 
@@ -85,9 +125,23 @@ function search() {
               "Humidity: " + data["current"].humidity + " %"
             );
             currentUV.text("UV Index: " + data["current"].uvi);
+
+            console.log("generating 5 day");
+            //five day
+            for (var i = 0; i < 5; i++) {
+              fiveDayIcon.push(data.daily[i].weather[0].icon);
+              fiveDayTemp.push(data.daily[i].temp.day);
+              fiveDayWind.push(data.daily[i].wind_speed);
+              fiveDayHumidity.push(data.daily[i].humidity);
+              console.log(fiveDayTemp);
+              console.log("5 day generated");
+              setDays(i);
+            }
           });
       });
   }
+
+  buttonPressed = false;
 }
 
 saveSearch();
@@ -131,3 +185,25 @@ function getWeather() {
 }
 
 //yippee
+
+function setDays(i) {
+  console.log(i + "setDays called");
+
+  $(".wincon").each(function (i) {
+    var code = fiveDayIcon[i];
+    var iconURL = "https://openweathermap.org/img/w/" + code + ".png";
+    $(this).attr("src", iconURL);
+  });
+  $(".dayTemp").each(function (i) {
+    var t = fiveDayTemp[i];
+    $(this).text(t + " °F");
+  });
+  $(".dayWind").each(function (i) {
+    var t = fiveDayWind[i];
+    $(this).text(t + " MPH");
+  });
+  $(".dayHumid").each(function (i) {
+    var t = fiveDayHumidity[i];
+    $(this).text(t + " %");
+  });
+}
